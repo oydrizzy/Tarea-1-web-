e400987015780e75d36bbd1388f491f5a6a64a2a
Daniel Paulino <166843475+oydrizzy@users.noreply.github.com>
2024-09-15 10:46:41 -0400

    actualizacion

--- obj/Debug/net8.0/codigos.AssemblyInfo.cs
+++ obj/Debug/net8.0/codigos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("codigos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25aaaee9829a3e8cc77506597d2470cc35ebd06e")]
 [assembly: System.Reflection.AssemblyProductAttribute("codigos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("codigos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
